aec25af4e4100beae81e3115d5c443be4c54c6c6
Kiko Guan <70984203+kikokills17@users.noreply.github.com>
2021-03-11 19:06:13 +0800

    revision

--- js/script.js
+++ js/script.js
@@ -1,162 +1,105 @@
-window.onload = function () {
-  let title = document.getElementsByClassName('work-title')[0];
-  let picture = document.getElementsByClassName('work-picture')[0];
-  let btns = Array.from(document.querySelectorAll('.footer>.footer-btns>li>a'));
-  let workInfo1 = [
-    {
-      title: "THE DANCE OF FAMILY RELATIONS",
-      id: 1,
-      src: "Poor traits/THE DANCE OF FAMILY RELATIONS.png"
-    },
-    {
-      title: "THE POUT",
-      id: 2,
-      src: "Poor traits/THE POUT.png"
-    },
-    {
-      title: "RAVEN HAIRED BEAUTY WITH WHITE FLOWER",
-      id: 3,
-      src: "Poor traits/RAVEN HAIRED BEAUTY WITH WHITE FLOWER.png"
-    },
-    {
-      title: "PINK WOMAN ASCENDING",
-      id: 4,
-      src: "Poor traits/PINK WOMAN ASCENDING.png"
-    },
-    {
-      title: "THE ROSE CEREMONY",
-      id: 5,
-      src: "Poor traits/THE ROSE CEREMONY.png"
-    },
-    {
-      title: "MORNING GEISHA",
-      id: 6,
-      src: "Poor traits/MORNING GEISHA.png"
-    },
-    {
-      title: "LUCY IN THE SKY WITH FLOWERS",
-      id: 7,
-      src: "Poor traits/LUCY IN THE SKY WITH FLOWERS.png"
-    },
-  ]
-  let workInfo2 = [
-    {
-      title: "SILICONE CLOUDS",
-      id: 1,
-      src: "in the nood/SILICONE CLOUDS.png"
-    },
-    {
-      title: "QUEEN OF HEARTS",
-      id: 2,
-      src: "in the nood/QUEEN OF HEARTS.png"
-    },
-    {
-      title: "MRS.SPOCK",
-      id: 3,
-      src: "in the nood/MRS.SPOCK.png"
-    },
-    {
-      title: "JAMES THE MALE MODERL",
-      id: 4,
-      src: "in the nood/JAMES THE MALE MODERL.png"
-    },
-    {
-      title: "CHIQUITA",
-      id: 5,
-      src: "in the nood/CHIQUITA.png"
-    },
-    {
-      title: "AMAZONS AT PLAY",
-      id: 6,
-      src: "in the nood/AMAZONS AT PLAY.png"
-    },
-    {
-      title: "THE BETTER TO SEE YOU MY DEAR",
-      id: 7,
-      src: "in the nood/THE BETTER TO SEE YOU MY DEAR.png"
-    },
-  ]
-  let workInfo3 = [
-    {
-      title: "PALS",
-      id: 1,
-      src: "the moba zoo/PALS.png"
-    },
-    {
-      title: "NON-SEEING EYE DOG",
-      id: 2,
-      src: "the moba zoo/NON-SEEING EYE DOG.png"
-    },
-    {
-      title: "FERRET IN A BROTHERL",
-      id: 3,
-      src: "the moba zoo/FERRET IN A BROTHERL.png"
-    },
-    {
-      title: "BLUE TANGO",
-      id: 4,
-      src: "the moba zoo/BLUE TANGO.png"
-    },
-    {
-      title: "UN POISSON MORT",
-      id: 5,
-      src: "the moba zoo/UN POISSON MORT.png"
-    },
-    {
-      title: "HE WAS A FRIEND OF MINE",
-      id: 6,
-      src: "the moba zoo/HE WAS A FRIEND OF MINE.png"
-    },
-    {
-      title: "PETER THE KITTY",
-      id: 7,
-      src: "the moba zoo/PETER THE KITTY.png"
-    },
-  ]
-  function getCollectionName (dom) {
-    return dom.getAttribute("data-name");
+let title = document.getElementsByClassName('work-title')[0];
+let picture = document.getElementsByClassName('work-picture')[0];
+let works = Array.from(picture.getElementsByTagName('img'));
+let btns = Array.from(document.getElementsByClassName('btnNum'));
+let collectionName = title.getAttribute('data-name');
+
+function changeTitle (name, index) {
+  if (name === 'poor') {
+    switch(index) {
+      case 0:
+        title.innerHTML = "THE DANCE OF FAMILY RELATIONS";
+        break;
+      case 1:
+        title.innerHTML = "THE POUT";
+        break;
+      case 2:
+        title.innerHTML = "RAVEN HAIRED BEAUTY WITH WHITE FLOWER";
+        break;
+      case 3:
+        title.innerHTML = "PINK WOMAN ASCENDING";
+        break;
+      case 4:
+        title.innerHTML = "THE ROSE CEREMONY";
+        break;
+      case 5:
+        title.innerHTML = "MORNING GEISHA";
+        break;
+      case 6:
+        title.innerHTML = "LUCY IN THE SKY WITH FLOWERS";
+        break;
+      default:
+        title.innerHTML = "POOR TRAITS";
+        break;
+    }
+  } else if (name === 'nood') {
+    switch(index) {
+      case 0:
+        title.innerHTML = "SILICONE CLOUDS";
+        break;
+      case 1:
+        title.innerHTML = "QUEEN OF HEARTS";
+        break;
+      case 2:
+        title.innerHTML = "MRS.SPOCK";
+        break;
+      case 3:
+        title.innerHTML = "JAMES THE MALE MODERL";
+        break;
+      case 4:
+        title.innerHTML = "CHIQUITA";
+        break;
+      case 5:
+        title.innerHTML = "AMAZONS AT PLAY";
+        break;
+      case 6:
+        title.innerHTML = "THE BETTER TO SEE YOU MY DEAR";
+        break;
+      default:
+        title.innerHTML = "IN THE NOOD";
+        break;
+    }
+  } else if (name === 'zoo') {
+    switch(index) {
+      case 0:
+        title.innerHTML = "PALS";
+        break;
+      case 1:
+        title.innerHTML = "NON-SEEING EYE DOG";
+        break;
+      case 2:
+        title.innerHTML = "FERRET IN A BROTHERL";
+        break;
+      case 3:
+        title.innerHTML = "BLUE TANGO";
+        break;
+      case 4:
+        title.innerHTML = "UN POISSON MORT";
+        break;
+      case 5:
+        title.innerHTML = "HE WAS A FRIEND OF MINE";
+        break;
+      case 6:
+        title.innerHTML = "PETER THE KITTY";
+        break;
+      default:
+        title.innerHTML = "THE MOBA ZOO";
+        break;
+    }
   }
-  let collectionName = getCollectionName(title);
+}
 
-  (function initialDraw() {
-    if (collectionName === "poor") {
-      title.innerHTML = "POOR TRAITS";
-    } else if (collectionName === "nood") {
-      title.innerHTML = "IN THE NOOD";
-    } else {
-      title.innerHTML = "THE MOBA ZOO";
-    }
-  }());
-
-  function draw (id) {
-    if (picture.children.length > 0) {
-      picture.removeChild(picture.children[0]);
-    }
-    let img = document.createElement('img');
-    let target = [];
-    if (collectionName === "poor") {
-      target = workInfo1.filter(e => {
-        return e.id == id;
-      })
-    } else if (collectionName === "nood") {
-      target = workInfo2.filter(e => {
-        return e.id == id;
-      })
-    } else {
-      target = workInfo3.filter(e => {
-        return e.id == id;
-      })
-    }
-    
-    img.src = `/img/${target[0].src}`;
-    picture.appendChild(img);
-    title.innerHTML = target[0].title;
-  }
-
-  btns.forEach(a => {
-    a.addEventListener('click', function(e) {
-      var id = e.target.innerHTML;
-      draw(id);
-    })
+function displayWork (e) {
+  let index = e.target.innerHTML - 1;
+  works.forEach(img => {
+    let classList = Array.from(img.classList);
+    if (classList.indexOf('active') === -1) return;
+    img.classList.remove('active');
   })
+  works[index].classList.add('active');
+  changeTitle(collectionName, index);
+}
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('mouseover', displayWork);
 }
